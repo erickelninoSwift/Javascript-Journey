e501511495af5990eb802a2f9904da2c6ac92010
Erick El nino <erick.tshimbombo@yahoo.com>
2023-03-26 20:14:10 +0200

    Update Array with javascript

--- JS15/main.js
+++ JS15/main.js
@@ -23,3 +23,18 @@ console.log(myArray);
 console.log(myArray[1]);
 
 
+const myArrayData = ["Eriik",34,false];
+
+console.log(myArrayData);
+
+myArrayData.splice(1,1, 333);
+
+console.log(myArrayData);
+
+const newArray = ['A','B','C','D','E','F','G'];
+
+const erickArray = newArray.slice(3);
+
+console.log(erickArray);
+
+
