04166fa557f83c51cdb1d3a6f4e69efc7adc39d9
Erick El nino <erick.tshimbombo@yahoo.com>
2023-03-27 11:42:53 +0200

    Chapter 17 bObjects

--- JS16/main.js
+++ JS16/main.js
@@ -10,18 +10,68 @@ const playgames = function()
 {
     while(true)
     {
-        const gameArray = ["rock","papers","scissors"];
+       
     
        let playerchoice = getPlayerChoice();
-
+      
        let theOnePlayerChoice = formatPlayerChoice(playerchoice);
+
        if (theOnePlayerChoice === "")
        {
-            console.log(playerchoice);
+            invalideChoice();
+            continue;
        }
+
+       if(!theOnePlayerChoice)
+       {
+         decidedNottoPlay()
+         break;
+       }
+
+       let evaluatePlayer = eveluatePlayersChoice(playerchoice)
+       if(!evaluatePlayer)
+       {
+         invalideChoice();
+         continue;
+       }
+
+       let compluterChoice = mycomputerChoice();
+       console.log(`Computer Choice is : ${compluterChoice}`);
+
+       let myResut = determinWinner(playerchoice,compluterChoice);
+       console.log(`The winner is : ${myResut}`);
+       
+       if(askToplayAgain())
+       {
+         continue;
+       }else
+       {
+        thanksforplaying();
+         break;
+       }
+ 
+
+
        
     }
 
+}
+
+const eveluatePlayersChoice = function(getPlayerChoice)
+{
+    const gameArray = ["rock","papers","scissors"];
+
+    if(getPlayerChoice.trim().toLowerCase() === gameArray[0].toLowerCase()
+        || getPlayerChoice.trim().toLowerCase() === gameArray[1].toLowerCase()
+        || getPlayerChoice.trim().toLowerCase() === gameArray[2].toLowerCase())
+        {
+
+            return getPlayerChoice;
+
+        }else
+        {
+            return false;
+        }
 }
 
 
@@ -38,49 +88,51 @@ const formatPlayerChoice = function(getPlayerChoice)
         return getPlayerChoice.trim().toLowerCase();
     }else
     {
+        thanksforplaying();
         return false;
     }
 }
 
+const decidedNottoPlay = function()
+{
+    return alert("I guess you have changed your mind maybe next time");
+}
+
+const invalideChoice = function()
+{
+    return  alert("you didnt enter rock , papers, scissors");
+}
+
+
+const mycomputerChoice = function()
+{
+    const gameArray = ["rock","papers","scissors"];
+    const computerPlayer = Math.floor(Math.random() * 3 + 1);
+    console.log(`Computer number : ${computerPlayer - 1} and Array : ${gameArray[computerPlayer - 1]}`);
+
+    return gameArray[computerPlayer - 1];
+}
+
+
+const determinWinner = function(playerChoice,compluterChoice)
+{
+    let result = playerChoice === compluterChoice ? "Tie game!" :
+    playerChoice === "rock" && compluterChoice === "papers" ? "computer Wins" :
+    playerChoice === "papers" && compluterChoice === "scissors" ? "Compter Wins" :
+    playerChoice === "scissors" && compluterChoice === "rock" ? "Computer Wins !" :
+                    "Player one Wins!"; 
+
+    return result;
+}
+
+const askToplayAgain = function()
+{
+    return confirm("Do you want to play again ?");
+}
+
+const thanksforplaying = function()
+{
+    return alert("Thanks for playing again");
+}
 
 initGame();
-
-
-// function All()
-// {
-//     if (formatPlayerChoice(playerchoice))
-//     {
-//         if(playerchoice.trim().toLowerCase() === gameArray[0].toLowerCase()
-//         || playerchoice.trim().toLowerCase() === gameArray[1].toLowerCase()
-//         || playerchoice.trim().toLowerCase() === gameArray[2].toLowerCase())
-//         {
-//             const playerOne = playerchoice.trim().toLowerCase();
-          
-//                 const computerPlayer = Math.floor(Math.random() * 3 + 1);
-//                 const computer = computerPlayer === 1 ? "rock" : 
-//                 computerPlayer === 2 ? "paper" : "scissors";
-
-//                 const result = playerOne === computer ? "Tie game!" :
-//                 playerOne === "rock" && computer === "papers" ? "computer Wins" :
-//                 playerOne === "papers" && computer === "scissors" ? "Compter Wins" :
-//                 playerOne === "scissors" && computer === "rock" ? "Computer Wins !" :
-//                 "Player one Wins!";
-
-//                 alert(result);
-
-//                 playgame = confirm("Do you want to play again ?");
-
-//                 if(!playgame) alert("Thanks for playing");
-//                 continue;
-//         }else
-//         {
-//             alert("you didnt enter rock , papers, scissors");
-//             continue;
-//         }
-
-//     }else
-//     {
-//         alert("I guess you have changed your mind maybe next time");
-//         break;
-//     }
-// }
