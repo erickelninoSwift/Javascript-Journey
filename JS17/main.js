32d83a1ed4d43b7e8ac942bf9345d32b9a8c5a66
Erick El nino <erick.tshimbombo@yahoo.com>
2023-03-27 13:49:25 +0200

    Object update

---
+++ JS17/main.js
@@ -0,0 +1,11 @@
+const myObject = {
+    alive:true,
+    myHobby: 45,
+    money: ["Erick","Elnino","jackpot"],
+    beverage: {
+        morning: "Coffee",
+        Afternoon: "ice tea"
+    }
+}
+
+console.log()
