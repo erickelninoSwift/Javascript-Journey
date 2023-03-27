e25e53dbb0dcf37e7c83b5d591574897dd3b45bd
Erick El nino <erick.tshimbombo@yahoo.com>
2023-03-27 14:03:14 +0200

    Object updated

--- JS17/main.js
+++ JS17/main.js
@@ -5,7 +5,20 @@ const myObject = {
     beverage: {
         morning: "Coffee",
         Afternoon: "ice tea"
+    },
+    engine: function()
+    {
+        return `What i drink in the morning ${this.beverage.morning}`;
     }
 }
 
-console.log()
+console.log(myObject.engine());
+
+const truck = Object.create(myObject);
+truck.money = function()
+{
+    return "Hello World";
+}
+
+console.log(truck.money());
+console.log(truck.alive);
