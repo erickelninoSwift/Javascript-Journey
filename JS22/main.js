252c5354055d9f0d66d7be9a65376047d9d7247b
Erick El nino <erick.tshimbombo@yahoo.com>
2023-03-30 01:08:03 +0200

    First commit

--- JS22/main.js
+++ JS22/main.js
@@ -1,1 +1,21 @@
 const myfirstView = document.querySelector("#view1");
+const secondView = document.querySelector("#view2")
+myfirstView.style.display = "none";
+secondView.style.display = "flex";
+const navbar = document.querySelector("nav");
+navbar.style.display = "none";
+
+const view = document.querySelector("#view2");
+const div = view.querySelector("div");
+const h2 = div.querySelector("h2");
+
+console.log(h2);
+
+const doSomething = function()
+{
+    alert("Hello jackpot here !");
+}
+
+h2.addEventListener("click",(e)=>{
+    doSomething();
+});
