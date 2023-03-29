11ce69f468619a3647656383799c8bbd81eb51ab
Erick El nino <erick.tshimbombo@yahoo.com>
2023-03-29 14:40:09 +0200

    Update

--- JS21/main.js
+++ JS21/main.js
@@ -1,2 +1,14 @@
-console.log("DOM CHAPTER 21");
 
+
+const view1 = document.getElementById("view1");
+console.log(view1);
+const view2 = document.querySelector("#view2");
+console.log(view2);
+view1.style.display = "flex";
+view2.style.display = "none";
+
+const views = document.getElementsByClassName("view");
+console.log(views);
+
+const elninoView = document.querySelectorAll('.view');
+console.log(elninoView);
