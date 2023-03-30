ac17cf7f8ccbca7f0b83fcaf0a68ab4317c8e881
Erick El nino <erick.tshimbombo@yahoo.com>
2023-03-30 18:41:11 +0200

    Modules

---
+++ JS24/js/guitar.js
@@ -0,0 +1,17 @@
+
+console.log("This is the guitar javascript file");
+
+export default function playGuitar ()
+{
+    return "Playing guitar";
+}
+
+export const shredding = () => {
+    return "Shredding some licks";
+}
+
+
+export const plucking = () => {
+
+    return "Plucking the strings";
+}
---
+++ JS24/js/main.js
@@ -0,0 +1,7 @@
+
+import playGuitar from "./guitar";
+import { shredding , plucking } from "./guitar";
+
+console.log(playGuitar());
+console.log(shredding());
+console.log(plucking());
