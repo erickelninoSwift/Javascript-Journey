c65bc26dca716539f11da7b69d9aaf9e6b877b1c
Erick El nino <erick.tshimbombo@yahoo.com>
2023-03-30 03:30:36 +0200

    Update event listener

--- JS22/main.js
+++ JS22/main.js
@@ -5,11 +5,6 @@ secondView.style.display = "flex";
 const navbar = document.querySelector("nav");
 navbar.style.display = "none";
 
-const view = document.querySelector("#view2");
-const div = view.querySelector("div");
-const h2 = div.querySelector("h2");
-
-console.log(h2);
 
 const doSomething = (text) =>
 {
@@ -29,11 +24,40 @@ const message = loveMore();
 // })
 
 
-h2.addEventListener("click",(event) =>{
+// h2.addEventListener("click",(event) =>{
    
-   event.target.textContent = event.target.textContent === "Jackpot!" ? "Click!" : "Jackpot!";
-})
+//    event.target.textContent = event.target.textContent === "Jackpot!" ? "Click!" : "Jackpot!";
+// })
+
+document.addEventListener("readystatechange",(myevent) =>{
+    if (myevent.target.readyState === "complete")
+    {
+        console.log("ReadyState :  Complete");
+        initApp();
+    }
+});
 
 // h2.addEventListener("click",(e)=>{
 //     doSomething();
 // });
+
+const initApp = () => {
+
+    const view = document.querySelector("#view2");
+    const div = view.querySelector("div");
+    const h2 = div.querySelector("h2");
+
+    view.addEventListener("click",(event) => {
+        view.style.backgroundColor = "red";
+    })
+
+    div.addEventListener("click",(event) => {
+        div.style.backgroundColor = "blue";
+    })
+
+    h2.addEventListener("click",(event) => {
+        event.target.textContent = event.target.textContent === "Jackpot!" ? "Clicked!" : "Jackpot!";
+
+    })
+
+};
