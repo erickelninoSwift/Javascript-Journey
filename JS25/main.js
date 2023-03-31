cd9a1c40dabc999b8f29436141a43790ce751099
Erick El nino <erick.tshimbombo@yahoo.com>
2023-03-31 03:26:21 +0200

    Filter functions

--- JS25/main.js
+++ JS25/main.js
@@ -1,3 +1,17 @@
 import {allmyPosts as posts} from "./post.js";
 
-console.log(posts);
+// console.log(posts);
+
+posts.forEach(post =>{
+    console.log(post);
+});
+
+const filteredPost = posts.filter(post =>{
+     return post.userid === 2;
+});
+
+filteredPost.forEach(post =>{
+    console.log("===============================");
+    console.log(post);
+    console.log("===============================");
+});
