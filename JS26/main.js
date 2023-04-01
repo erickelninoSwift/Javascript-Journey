7cbfb2b4441b7424783ecdd2232837c7b323f59b
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-01 22:57:41 +0200

    Promise

--- JS26/main.js
+++ JS26/main.js
@@ -1,1 +1,27 @@
-console.log("Callbacks")
+
+const myPromise = new Promise((resolve,reject) => {
+  const erro = false;
+  if(!erro)
+  {
+    resolve(30 + 30);
+  }else
+  {
+    reject("Hello this is jackpot here")
+  }
+});
+
+console.log(myPromise);
+
+const erickPorimise = new Promise((resolve, reject) =>{
+
+    const number = prompt("Please enter your age : ");
+    if (number >= 18)
+    {
+        resolve("you are very mature")
+    }else
+    {
+        reject("You are very young");
+    }
+});
+
+console.log(erickPorimise);
