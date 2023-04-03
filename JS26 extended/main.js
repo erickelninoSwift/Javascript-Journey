b713888d5762a460de6fa49b8732e637f4efe0ae
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-03 16:35:51 +0200

    Update messsage

--- JS26 extended/main.js
+++ JS26 extended/main.js
@@ -1,1 +1,26 @@
 console.log("Chapter 26 extended Async / Await");
+
+const myPromise = new Promise((resolve,reject) =>{
+    const error = false;
+    if(!error)
+    {
+        resolve("How are you doing ? My firned");
+    }else
+    {
+        reject("Error !!!!");
+    }
+});
+
+myPromise.then(response =>{
+
+    return response + "Eriik";
+
+}).then(result =>{
+    console.log(result);
+
+}).catch(error =>{
+
+    console.log(error);
+});
+
+
