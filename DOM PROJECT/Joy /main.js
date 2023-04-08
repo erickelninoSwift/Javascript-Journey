90a9ba7d1556233c451feac9a75b75b200da800d
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-08 22:29:24 +0200

    Update proejct

--- DOM PROJECT/Joy /main.js
+++ DOM PROJECT/Joy /main.js
@@ -1,79 +1,79 @@
-console.log("I");
-console.log("eat");
-console.log("Ice Cream");
-console.log("With");
-console.log("Spoon");
+// console.log("I");
+// console.log("eat");
+// console.log("Ice Cream");
+// console.log("With");
+// console.log("Spoon");
 
-// CallBacks
+// // CallBacks
 
-function one(call_two)
-{
-    console.log("my Step one is completed , Please call step 2");
-    call_two();
-}
-
-
-function two()
-{
-    console.log("Step too also completed now now");
-}
-
-one(two);
+// function one(call_two)
+// {
+//     console.log("my Step one is completed , Please call step 2");
+//     call_two();
+// }
 
 
-let stocks = {
-    Fruits:["Mango","Banana","Orange"],
-    Liquid:["Milk","Juice","Yoghurt"],
-    Food:["Salads","Cucumber","Chicken"],
-    Cup:["Plate","bowl","Cup"],
-    Shoes:["Reebook","Nike","Puma"]
-}
+// function two()
+// {
+//     console.log("Step too also completed now now");
+// }
+
+// one(two);
+
+
+// let stocks = {
+//     Fruits:["Mango","Banana","Orange"],
+//     Liquid:["Milk","Juice","Yoghurt"],
+//     Food:["Salads","Cucumber","Chicken"],
+//     Cup:["Plate","bowl","Cup"],
+//     Shoes:["Reebook","Nike","Puma"]
+// }
 
 
 
-let order = (fruits_name,call_production) =>{
+// let order = (fruits_name,call_production) =>{
 
-    setTimeout(() =>{
-         console.log(`Received order : ${fruits_name} was selected`);
-         call_production();
-    },2000);
+//     setTimeout(() =>{
+//          console.log(`Received order : ${fruits_name} was selected`);
+//          call_production();
+//     },2000);
 
         
-};
+// };
 
-let production = () =>{
-    setTimeout(() =>{
-        console.log("Production has started");
+// let production = () =>{
+//     setTimeout(() =>{
+//         console.log("Production has started");
 
-        setTimeout(() =>{
-            console.log("The food have been chopped");
+//         setTimeout(() =>{
+//             console.log("The food have been chopped");
 
-            setTimeout(() =>{
-                console.log(`${stocks.Liquid[0]} and ${stocks.Liquid[1]} was added !`);
+//             setTimeout(() =>{
+//                 console.log(`${stocks.Liquid[0]} and ${stocks.Liquid[1]} was added !`);
                  
-                setTimeout(() =>{
-                    console.log("The machine have been started!!");
-                    setTimeout(() =>{
+//                 setTimeout(() =>{
+//                     console.log("The machine have been started!!");
+//                     setTimeout(() =>{
 
-                        console.log(`The container is : ${stocks.Cup[0]}`);
+//                         console.log(`The container is : ${stocks.Cup[0]}`);
 
-                        setTimeout(() =>{
-                            console.log(`Toppings : ${stocks.Fruits[2]}`);
+//                         setTimeout(() =>{
+//                             console.log(`Toppings : ${stocks.Fruits[2]}`);
 
-                            setTimeout(() =>{
-                                console.log("Food is served !");
-                            },2000);
-                        },3000);
+//                             setTimeout(() =>{
+//                                 console.log("Food is served !");
+//                             },2000);
+//                         },3000);
 
-                    },2000);
-                },1000);
-            },1000);
-        },2000);
-    });
-};
+//                     },2000);
+//                 },1000);
+//             },1000);
+//         },2000);
+//     });
+// };
 
 
-order(stocks.Food[1],production);
+// order(stocks.Food[1],production);
 
 
 
@@ -95,7 +95,7 @@ const myOrder = (time,work) =>{
         if(!is_Wordk_open)
         {
             setTimeout(() =>{
-                resolve(work());
+               resolve(work());
             },time);
         }else
         {
@@ -108,15 +108,27 @@ const mywork = () =>{
     return `${stockShops.Fruits[2]} was selected !`;
 };
 
-myOrder(2000,mywork).then(value =>{
-    console.log(value);
-}).catch(error =>{
-    console.log(`Error found : ${error}`);
-});
+myOrder(2000,mywork).then(() =>{
+    return myOrder(1000,() => console.log(`${stockShops.Fruits[0]} was selected!`));
+})
+.then(() =>{
+    return myOrder(2000, () => console.log(`Production have started !`));
+}).then(() =>{
+    return myOrder(1000,() => console.log(`Add water and Ice`));
+})
+.then(() =>{
+    return myOrder(2000,() => console.log(`Add some ${stockShops.Liquid[0]}`));
+})
+.then(() =>{
+    return myOrder(1000,() => console.log(`Container : ${stockShops.Shoes[0]}`));
+})
 
-// myOrder(1000,() =>`${stockShops.Fruits[2]} was selected !`).then(value =>{
-//     console.log(value);
-// });
+.then(() =>{
+
+    return myOrder(2000,() =>{
+        console.log(`Ice cream was served !!!`);
+    });
+});
 
 // myOrder(2000,function elnino(){
 //     console.log(`Erickelnino jackpot!!`);
