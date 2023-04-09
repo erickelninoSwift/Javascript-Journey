3fe62dabbdb1091d4d2414de5e66b2f39b7f53ee
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-09 15:12:43 +0200

    Update async await function

--- DOM PROJECT/Joy /async.js
+++ DOM PROJECT/Joy /async.js
@@ -1,2 +1,20 @@
 console.log("Hello world");
 
+const order = async (() =>{
+    try
+    {
+    
+
+    }catch(error)
+    {
+        console.log("AbC doesnt exist ,", error);
+    }finally
+    {
+        console.log("Its runs anyways");
+    }
+});
+
+
+order.then(() =>{
+    console.log("We are more solid than we ever been");
+});
