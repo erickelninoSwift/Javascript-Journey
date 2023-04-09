e70e775566f385e4756b31b9374cfe5ce4d67d96
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-09 16:55:41 +0200

    Update All data

--- DOM PROJECT/Joy /async.js
+++ DOM PROJECT/Joy /async.js
@@ -1,20 +1,77 @@
 console.log("Hello world");
 
-const order = async (() =>{
+// const order = async (() =>{
+//     try
+//     {
+    
+
+//     }catch(error)
+//     {
+//         console.log("AbC doesnt exist ,", error);
+//     }finally
+//     {
+//         console.log("Its runs anyways");
+//     }
+// });
+
+
+// async function order()
+// {
+//     try
+//     {
+//         await abc;
+
+//     }catch(error)
+//     {
+//         console.log("Error was found : ", error);
+
+//     }finally
+//     {
+//         console.log("Shop is closed Thank you !");
+//     }
+// }
+
+// order();
+
+let imaMature = true;
+
+const makeAppleJuice = () =>{
+    return new Promise((resolve,reject) =>{
+
+        if (imaMature)
+        {
+            setTimeout(() =>{
+                resolve(console.log(`I am old enough to have a child`));
+            },3000);
+
+        }else
+        {
+           reject(console.log(`Error sorry you still very youg`)) 
+        }
+
+    });
+}
+
+
+const mysecondJob = async () =>{
     try
     {
-    
+        console.log("I am erick");
+        console.log("I love to eat Pasta");
+        console.log("With Wine");
+
+        await makeAppleJuice();
+
+        console.log("Bonne appetit");
+
 
     }catch(error)
     {
-        console.log("AbC doesnt exist ,", error);
+
     }finally
     {
-        console.log("Its runs anyways");
+        console.log(`Run everything anyways`);
     }
-});
+}
 
-
-order.then(() =>{
-    console.log("We are more solid than we ever been");
-});
+mysecondJob();
